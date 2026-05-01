aeabb8d0d1b8516b1be0fc57bec229972ca03236
Stefan Jeitler <stefan.jeitler@outlook.com>
2026-05-01 22:51:19 +0200

    card padding removed

--- src/components/ui/display/card.tsx
+++ src/components/ui/display/card.tsx
@@ -33,6 +33,7 @@ function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
       data-slot="card-header"
       className={cn(
         'group/card-header @container/card-header grid auto-rows-min items-start gap-1 rounded-t-xl px-4 group-data-[size=sm]/card:px-3 has-data-[slot=card-action]:grid-cols-[1fr_auto] has-data-[slot=card-description]:grid-rows-[auto_auto] [.border-b]:pb-4 group-data-[size=sm]/card:[.border-b]:pb-3',
+        'group-data-[variant=default]/card:px-0',
         className,
       )}
       {...props}
@@ -80,7 +81,10 @@ function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="card-content"
-      className={cn('px-4 group-data-[size=sm]/card:px-3', className)}
+      className={cn(
+        'px-4 group-data-[size=sm]/card:px-3 group-data-[variant=default]/card:px-0',
+        className,
+      )}
       {...props}
     />
   )
@@ -92,7 +96,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
       data-slot="card-footer"
       className={cn(
         'flex flex-col-reverse flex-wrap items-stretch justify-end gap-6 rounded-b-xl p-4 group-data-[size=sm]/card:p-3 @sm:flex-row @sm:items-center',
-        'group-data-[variant=muted]/card:bg-muted/50',
+        'group-data-[variant=muted]/card:bg-muted/50 group-data-[variant=default]/card:px-0',
         className,
       )}
       {...props}
