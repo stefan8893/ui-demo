d9664a7048b561fa87e5341a65ed7db239ef9607
stefan8893 <stefan8893@users.noreply.github.com>
2026-05-15 19:36:22 +0200

    fix linter error

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -5,6 +5,5 @@ export function ThemeProvider({
 	children,
 	...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-	const foo = 'bar'
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
